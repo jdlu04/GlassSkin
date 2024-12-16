f123b43f8d988338c72281db7ed1324bea9dda01
jdlu04 <liujudy04@gmail.com>
2024-12-16 17:28:47 -0500

    product listing updates

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,39 +1,49 @@
 import React, { useEffect, useState } from 'react';
 
-const ProductCard = ({ productId }) => {
+
+const ProductCard = ({product}) => {
+  /*
   const [product, setProduct] = useState(null);
 
   useEffect(() => {
     const fetchProduct = async () => {
+      const params = new URLSearchParams(request);
+        
       try {
-        const response = await fetch(`http://127.0.0.1:5000/api/specific/${productId}`);
+        const response = await fetch(`http://127.0.0.1:5000/api/recommended_uncapped?${params.toString()}`);
         if (!response.ok) {
           throw new Error('Failed to fetch product');
         }
         const productData = await response.json();
-        setProduct(productData);
+        
+        if (productData) {
+          setProduct(productData[0]);
+          console.log(productData)
+        } else {
+          console.log('error')
+        }
+        
       } catch (error) {
         console.error("Error: ", error);
       }
     };
 
     fetchProduct();
-  }, [productId]);
-
+  }, []);
+*/
   return (
     product ? (
-      <button className="w-1/5 rounded-lg hover:shadow-2xl hover:bg-gray-200 ">
-        <img className ='px-14 py-5 m-1' src={`https:${product.api_featured_image}`} alt={product.name} />
-        <div className='px-10'>
+      <button className='w-1/4 rounded-lg hover:shadow-2xl hover:bg-gray-200 '>
+        <img className='p-5' src={`https:${product.api_featured_image}`} alt={product.name} />
+        <div>
           <h1>{product.brand}</h1>
           <h1>{product.name}</h1>
           <h1>${product.price}</h1>
-        
-          <div className="flex flex-wrap m-2">
+          <div className='flex flex-wrap m-2'>
             {product.product_colors.map((color, index) => (
               <div
                 key={index}
-                className="w-5 h-5 m-1 rounded-full"
+                className='w-5 h-5 m-1 rounded-full'
                 style={{ backgroundColor: color.hex_value }}
                 title={color.colour_name}
               />
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 import ProductCard from "../Components/ProductCard";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Heart } from 'lucide-react';
 
 const Products = () => {
-  const productIds = Array.from({ length: 60 }, (_, index) => index + 1);
-    return (
+  const productIds = Array.from({ length: 10 }, (_, index) => index + 1);
+  const [product, setProduct] = useState(null);
+  const [likedProducts, setLikedProducts] = useState([]);
+  const [heart, setHeart] = useState(false);
+  const [request] = useState({
+    brand: 'glossier'
+  });
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const params = new URLSearchParams(request);
+        
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/api/recommended_uncapped?${params.toString()}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch product');
+        }
+        const productData = await response.json();
+        
+        if (productData) {
+          setProduct(productData);
+          console.log(productData)
+        } else {
+          console.log('error')
+        }
+        
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+
+    fetchProduct();
+  }, [request]);
+
+  const setLiked = (id) => {
+    setLikedProducts((prevLikedProducts) => [...prevLikedProducts, id]);
+  }
+
+  return (
     <>
+    {/*
+    <div>
+      <h3>Debugging</h3>
+          <pre>{JSON.stringify(likedProducts, null, 2)}</pre>
+    </div>
+    */}
     <div className="min-h-75 w-screen bg-green">
       <div className="text-center py-20 px-10 text-8xl">
         <h1 className="fade-in">Curated for your look</h1>
@@ -14,13 +58,22 @@ const Products = () => {
         </h1>
       </div>
     </div>
-    <div className='w-screen p-5 flex flex-wrap justify-center gap-5'>
-      {productIds.map((productId) => (
-          <ProductCard key={productId} productId={productId} />
-        ))}
+    <div className='w-screen flex-wrap'>
+      {product ? (
+        product.map((productItem) => (
+          <div key={productItem.id}>
+            <button onClick={() => setLiked(productItem.id)}>
+              <Heart fill={heart ? 'red' : 'white'} />
+            </button>
+            <ProductCard request={request} product={productItem} /> {/* Pass the individual product */}
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
     </>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
