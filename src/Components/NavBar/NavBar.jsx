99cb64d6773884276cd133ecf13b25f06b564039
dannyr122 <dannyr151617@gmail.com>
2024-12-17 13:23:07 -0500

    update logout onclick

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -43,7 +43,10 @@ const NavBar = () => {
                 <span className="hidden sm:inline">Profile</span>
               </Link> */}
 
-              <button className="flex gap-2 items-center" onClick={logout}>
+              <button className="flex gap-2 items-center" onClick={() => {
+                logout()
+                navigate('/');}
+              }>
                 <LogOut className="size-5" />
                 <span className="hidden sm:inline">Logout</span>
               </button>
